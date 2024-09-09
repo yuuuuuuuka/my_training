2e1fff6c7f1fb4d7a85be4cf988c99c88c59833a
yuuuuuuuka <yuuuuka.02.29@gmail.com>
2024-09-09 19:46:44 +0900

    DB編集

--- db/migrate/db/migrate/20240908071023_records.rb
+++ db/migrate/db/migrate/20240908071023_records.rb
@@ -1,4 +1,4 @@
-class CreateRecords < ActiveRecord::Migration[7.0]
+class CreateTrainingRecords < ActiveRecord::Migration[7.0]
   def change
     create_table :records do |t|
       t.references :user,     null: false,foreign_key: true
